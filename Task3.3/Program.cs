358adb3d0a92c9030531d206cd153105d6e800ef
DenisV <naum2891@mail.ru>
2022-09-09 01:10:46 +0300

    add "Таблица числа N от 1 до N"

---
+++ Task3.3/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+int Prompt(string messeg)
+{
+    System.Console.WriteLine(messeg);
+    string value = System.Console.ReadLine() ?? "0";
+    int valueA = int.Parse(value);
+    return valueA;
+}
+int[] fankCub(int value)
+{
+    int[] array1 = new int[1];
+    if (value == 0)
+    {
+        array1[0] = 0;
+    }
+    int[] array = new int[Math.Abs(value)];
+    if (value > 0)
+    {
+        double count = 1;
+        for (int i = 0; i < value; i++)
+        {
+            array[i] = (int)Math.Pow(count, 3);
+            count++;
+        }
+    }
+    else
+    {
+        double count = -1;
+        for (int i = 0; i < Math.Abs(value); i++)
+        {
+            array[i] = (int)Math.Pow(count, 3);
+            count--;
+        }
+    }
+    if (value == 0)
+    {
+        return array1;
+    }
+    else
+    {
+        return array;
+    }
+
+
+}
+void concLusion(int[] array, int value)
+{
+    System.Console.Write($"Таблица кубов для числа {value} ->");
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write($" {array[i]},");
+    }
+    System.Console.WriteLine();
+}
+int valueB = Prompt("Введите число :");
+concLusion(fankCub(valueB), valueB);
