5c677279eaf20c0bd365eb30b570870876b6afe2
DenisV <naum2891@mail.ru>
2022-09-09 15:11:16 +0300

    add "Проверка числа на полиандромность без массивов "

---
+++ Task3.4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 1
+
+// Напишите программу, которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом. Не использовать строки!
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+int Prompt(string messeg)//Функция ввода числа
+{
+    System.Console.WriteLine(messeg);
+    string valueA = System.Console.ReadLine() ?? "0";
+    int valueB = int.Parse(valueA);
+    return valueB;
+}
+int Inversion(int value)
+{
+    value = Math.Abs(value);
+    int valueA = 0;
+    while(value > 0)
+    {
+        valueA = (valueA + value%10)*10;
+        value = (value - value%10)/10;
+    }
+    return valueA/10;
+}
+int value1 = Prompt("Введите число : ");
+int value2 = Inversion(value1);
+if (Math.Abs(value1) == value2)
+{
+    System.Console.WriteLine($"Число {value1} является полиандромом");
+}
+else
+{
+    System.Console.WriteLine($"Число {value1} не является полиандромом");
+}
